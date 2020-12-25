8d4029e41ea8792adfc40d58a526e57e2b9cb6e2
Andrew McOlash <amcolash@gmail.com>
2020-12-24 21:12:03 -0600

    move some things around, partially get tied notes working (though not actually tied)

--- index.js
+++ index.js
@@ -5,18 +5,35 @@ const cv = require('opencv4nodejs');
 const axios = require('axios');
 const im = require('imagemagick');
 
+const DEBUG = true;
+
+const dot = cv.imread('./templates/dot.png');
+const halfBar = cv.imread('./templates/half-bar.png');
+const halfBlank = cv.imread('./templates/half-blank.png');
+const quarter = cv.imread('./templates/quarter.png');
+const wholeBar = cv.imread('./templates/whole-bar.png');
+const wholeBlank = cv.imread('./templates/whole-blank.png');
+const eighth = cv.imread('./templates/eighth.png');
+const tieLeft = cv.imread('./templates/tie-left.png');
+const tieRight = cv.imread('./templates/tie-right.png');
+
+const templates = {
+  dot: { mat: dot, thresh: 0.25 },
+  halfBar: { mat: halfBar, thresh: 0.25 },
+  halfBlank: { mat: halfBlank, thresh: 0.25 },
+  quarter: { mat: quarter, thresh: 0.25 },
+  wholeBar: { mat: wholeBar, thresh: 0.25 },
+  wholeBlank: { mat: wholeBlank, thresh: 0.25 },
+  eighth: { mat: eighth, thresh: 0.2 },
+  tieLeft: { mat: tieLeft, thresh: 0.25 },
+};
+
+const tieRightTemplate = { mat: tieRight, thresh: 0.25 };
+
 const song = JSON.parse(fs.readFileSync('test.json').toString()).data.song;
 parseSong(song);
 
-function lerp(v0, v1, t) {
-  return v0 * (1 - t) + v1 * t;
-}
-
-function clamp(min, max, value) {
-  return Math.max(min, Math.min(max, value));
-}
-
-function getDuration(matches) {
+function getDuration(matches, extraDuration) {
   let duration = '8';
   if (matches.wholeBar || matches.wholeBlank) duration = '1';
   if (matches.halfBar || matches.halfBlank) duration = '2';
@@ -24,6 +41,7 @@ function getDuration(matches) {
   if (matches.eighth) duration = '8';
   if (matches.dot) duration = 'd' + duration;
 
+  if (extraDuration) return [duration, extraDuration];
   return duration;
 }
 
@@ -72,68 +90,89 @@ async function downloadImages(song) {
   });
 }
 
+function getMatchedTemplates(mat, templates) {
+  const matches = {};
+
+  Object.entries(templates).forEach((t) => {
+    const name = t[0];
+    const value = t[1];
+    const template = value.mat;
+
+    const match = mat.matchTemplate(template, cv.TM_CCOEFF_NORMED);
+    const minMax = match.minMaxLoc();
+
+    // if (DEBUG) console.log(name, minMax);
+
+    if (minMax.maxVal > 1 - value.thresh) {
+      matches[name] = { x: minMax.maxLoc.x, y: minMax.maxLoc.y };
+
+      if (DEBUG)
+        mat.drawRectangle(
+          new cv.Rect(minMax.maxLoc.x, minMax.maxLoc.y, template.cols, template.rows),
+          new cv.Vec3(0, 0, 255),
+          2,
+          cv.LINE_8
+        );
+    }
+  });
+
+  return matches;
+}
+
+function handleTies(mat, newCrop) {
+  const copiedCrop = newCrop.copy();
+  const match = getMatchedTemplates(copiedCrop, { tieRight: tieRightTemplate });
+
+  // if (DEBUG) cv.imshow('window', copiedCrop);
+  // if (DEBUG) cv.waitKey();
+
+  if (match.tieRight) {
+    const rightCrop = newCrop.getRegion(new cv.Rect(match.tieRight.x + 10, 0, 65, newCrop.rows));
+
+    const tieMatch = getMatchedTemplates(rightCrop, templates);
+
+    // if (DEBUG) cv.imshow('window', rightCrop);
+    // if (DEBUG) cv.waitKey();
+
+    return getDuration(tieMatch);
+  }
+}
+
 async function parseSong(song) {
   const notes = song.noteEvents;
 
   await downloadImages(song);
 
-  const dot = cv.imread('./templates/dot.png');
-  const halfBar = cv.imread('./templates/half-bar.png');
-  const halfBlank = cv.imread('./templates/half-blank.png');
-  const quarter = cv.imread('./templates/quarter.png');
-  const wholeBar = cv.imread('./templates/whole-bar.png');
-  const wholeBlank = cv.imread('./templates/whole-blank.png');
-  const eighth = cv.imread('./templates/eighth.png');
-
-  const templates = {
-    dot: { mat: dot, thresh: 0.25 },
-    halfBar: { mat: halfBar, thresh: 0.25 },
-    halfBlank: { mat: halfBlank, thresh: 0.25 },
-    quarter: { mat: quarter, thresh: 0.25 },
-    wholeBar: { mat: wholeBar, thresh: 0.25 },
-    wholeBlank: { mat: wholeBlank, thresh: 0.25 },
-    eighth: { mat: eighth, thresh: 0.2 },
-  };
-
   const mat = cv.imread('./tmp/output.png');
 
   const numNotes = 4;
   for (let i = 0; i < numNotes; i++) {
-    const cropped = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 65, mat.rows));
+    const cropped = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 65, mat.rows)).copy();
 
     const halfHeight = cropped.rows / 2;
     const leftBar = cropped.getRegion(new cv.Rect(0, halfHeight, cropped.cols, halfHeight));
     const rightBar = cropped.getRegion(new cv.Rect(0, 0, cropped.cols, halfHeight));
 
     [leftBar, rightBar].forEach((bar, j) => {
-      const tmpMat = bar.copy();
+      const matches = getMatchedTemplates(bar, templates);
 
-      const matches = {};
-
-      Object.entries(templates).forEach((t) => {
-        const name = t[0];
-        const value = t[1];
-        const mat = value.mat;
-
-        const match = tmpMat.matchTemplate(mat, cv.TM_SQDIFF_NORMED);
-        const minMax = match.minMaxLoc();
-
-        if (minMax.minVal < value.thresh) {
-          matches[name] = true;
-
-          // tmpMat.drawRectangle(new cv.Rect(minMax.minLoc.x, minMax.minLoc.y, mat.cols, mat.rows), new cv.Vec3(0, 0, 255), 2, cv.LINE_8);
-        }
-      });
+      let extraDuration;
+      if (matches.tieLeft) {
+        let newCrop;
+        if (j === 0) newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, halfHeight, 250, halfHeight));
+        else newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 250, halfHeight));
+        extraDuration = handleTies(mat, newCrop);
+      }
 
       // console.log(matches);
-      const duration = getDuration(matches);
+      const duration = getDuration(matches, extraDuration);
       // console.log(duration);
 
       if (j === 0) notes[i].ld = duration;
       if (j === 1) notes[i].rd = duration;
 
-      // cv.imshow('a window name', tmpMat);
-      // cv.waitKey();
+      // if (DEBUG) cv.imshow('window', bar);
+      // if (DEBUG) cv.waitKey();
     });
   }
 
@@ -152,16 +191,6 @@ async function parseSong(song) {
     min = Math.min(min, n.t);
   });
 
-  // for (let i = 0; i < leftNotes.length - 1; i++) {
-  //   leftNotes[i].d = leftNotes[i + 1].t - leftNotes[i].t;
-  // }
-  // leftNotes[leftNotes.length - 1].d = song.syncData.t[song.syncData.t.length - 1] - leftNotes[leftNotes.length - 1].t;
-
-  // for (let i = 0; i < rightNotes.length - 1; i++) {
-  //   rightNotes[i].d = rightNotes[i + 1].t - rightNotes[i].t;
-  // }
-  // rightNotes[rightNotes.length - 1].d = song.syncData.t[song.syncData.t.length - 1] - rightNotes[rightNotes.length - 1].t;
-
   const rightTrack = new MidiWriter.Track();
   const leftTrack = new MidiWriter.Track();
 
@@ -176,14 +205,18 @@ async function parseSong(song) {
     const ticks = ((n.t - min) / 1000) * tps;
     // const notes = n.notesR.map((r) => `${r.name.replace('♭', 'b').replace('♯', '#')}${r.octave}`);
     const notes = n.notesR.map((r) => r.key);
-    rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d, wait: 'T' + ticks }));
+    // rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d, wait: 'T' + ticks }));
+    if (Array.isArray(n.d)) n.d.forEach((d) => rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
+    else rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
   });
 
   leftNotes.forEach((n, i) => {
     const ticks = ((n.t - min) / 1000) * tps;
     // const notes = n.notesL.map((l) => `${l.name.replace('♭', 'b').replace('♯', '#')}${l.octave}`);
     const notes = n.notesL.map((l) => l.key);
-    leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d, wait: 'T' + ticks }));
+    // leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d, wait: 'T' + ticks }));
+    if (Array.isArray(n.d)) n.d.forEach((d) => leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
+    else leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
   });
 
   const write = new MidiWriter.Writer([rightTrack, leftTrack]);
