480b3f1c0270d44262fc5975753b207df0343749
Andrew McOlash <amcolash@gmail.com>
2020-12-26 16:19:15 -0600

    move things around, soldi progress on writing musicxml

--- src/index.js
+++ src/index.js
@@ -1,43 +1,52 @@
 const fs = require('fs');
 const path = require('path');
-const MidiWriter = require('midi-writer-js');
 const cv = require('opencv4nodejs');
 const axios = require('axios');
 const im = require('imagemagick');
+const toXml = require('./build');
 
 const DEBUG = true;
 
 const templates = {
-  dot: { mat: cv.imread('./templates/dot.png'), thresh: 0.25 },
-  halfBar: { mat: cv.imread('./templates/half-bar.png'), thresh: 0.25 },
-  halfBlank: { mat: cv.imread('./templates/half-blank.png'), thresh: 0.25 },
-  quarter: { mat: cv.imread('./templates/quarter.png'), thresh: 0.25 },
-  wholeBar: { mat: cv.imread('./templates/whole-bar.png'), thresh: 0.25 },
-  wholeBlank: { mat: cv.imread('./templates/whole-blank.png'), thresh: 0.25 },
-  eighth: { mat: cv.imread('./templates/eighth.png'), thresh: 0.2 },
-  tieLeft: { mat: cv.imread('./templates/tie-left.png'), thresh: 0.25 },
+  dot: { mat: loadImage('./templates/dot.png'), thresh: 0.25 },
+  halfBar: { mat: loadImage('./templates/half-bar.png'), thresh: 0.25 },
+  halfBlank: { mat: loadImage('./templates/half-blank.png'), thresh: 0.25 },
+  quarter: { mat: loadImage('./templates/quarter.png'), thresh: 0.25 },
+  wholeBar: { mat: loadImage('./templates/whole-bar.png'), thresh: 0.25 },
+  wholeBlank: { mat: loadImage('./templates/whole-blank.png'), thresh: 0.25 },
+  eighth: { mat: loadImage('./templates/eighth.png'), thresh: 0.2 },
+  tieLeft: { mat: loadImage('./templates/tie-left.png'), thresh: 0.25 },
 };
 
-const barsTemplate = { mat: cv.imread('./templates/bars.png'), thresh: 0.2 };
-const measureTemplate = { mat: cv.imread('./templates/measure.png'), thresh: 0.1 };
-const tieRightTemplate = { mat: cv.imread('./templates/tie-right.png'), thresh: 0.25 };
+const barsTemplate = { mat: loadImage('./templates/bars.png'), thresh: 0.2 };
+const measureTemplate = { mat: loadImage('./templates/measure.png'), thresh: 0.1 };
+const tieRightTemplate = { mat: loadImage('./templates/tie-right.png'), thresh: 0.25 };
 
 const timeSignatures = {
-  time_3_4: { mat: cv.imread('./templates/3-4.png'), thresh: 0.2 },
-  time_4_4: { mat: cv.imread('./templates/4-4.png'), thresh: 0.2 },
-  time_6_8: { mat: cv.imread('./templates/6-8.png'), thresh: 0.2 },
+  time_3_4: { mat: loadImage('./templates/3-4.png'), thresh: 0.2 },
+  time_4_4: { mat: loadImage('./templates/4-4.png'), thresh: 0.2 },
+  time_6_8: { mat: loadImage('./templates/6-8.png'), thresh: 0.2 },
 };
 
+function loadImage(p) {
+  return cv.imread(path.join(__dirname, p));
+}
+
 function getDuration(matches, extraDuration) {
-  let duration = '8';
-  if (matches.wholeBar || matches.wholeBlank) duration = '1';
-  if (matches.halfBar || matches.halfBlank) duration = '2';
-  if (matches.quarter) duration = '4';
-  if (matches.eighth) duration = '8';
-  if (matches.dot) duration = 'd' + duration;
+  let duration = 'eighth';
+  if (matches.wholeBar || matches.wholeBlank) duration = 'whole';
+  if (matches.halfBar || matches.halfBlank) duration = 'half';
+  if (matches.quarter) duration = 'quarter';
+  if (matches.eighth) duration = 'eighth';
 
   if (extraDuration) return [duration, extraDuration];
-  return duration;
+  return { duration, dot: matches.dot !== undefined };
+}
+
+function getTimeSignature(matches) {
+  if (matches.time_3_3) return { top: 3, bottom: 4 };
+  if (matches.time_4_4) return { top: 4, bottom: 4 };
+  if (matches.time_6_8) return { top: 6, bottom: 8 };
 }
 
 async function downloadImages(song) {
@@ -70,11 +79,11 @@ async function downloadImages(song) {
 
   await Promise.all(imgs);
 
-  const combined = 'tmp/output.png';
+  const combined = path.join(__dirname, '../tmp/output.png');
   if (fs.existsSync(combined)) fs.unlinkSync(combined);
   const files = fs
-    .readdirSync('tmp/')
-    .map((f) => 'tmp/' + f)
+    .readdirSync(path.join(__dirname, '../tmp/'))
+    .map((f) => path.join(__dirname, '../tmp/', f))
     .sort((a, b) => a.localeCompare(b, 'en', { numeric: true }));
 
   await new Promise((resolve, reject) => {
@@ -151,7 +160,7 @@ function handleTies(newCrop) {
   }
 }
 
-function getMeasures(mat) {
+function getMeasures(mat, notes) {
   const rightBar = mat.getRegion(new cv.Rect(0, 0, mat.cols, mat.rows / 2));
   const rightBarMatch = getMatchedTemplates(rightBar.copy(), { bars: barsTemplate });
   const rightCrop = rightBar.getRegion(new cv.Rect(0, rightBarMatch.bars.y, mat.cols, barsTemplate.mat.rows));
@@ -172,8 +181,14 @@ function getMeasures(mat) {
   const measures = filteredMeasure
     .sort((a, b) => a.x - b.x)
     .map((m, i) => {
-      return { x1: m.x, x2: i < filteredMeasure.length - 1 ? filteredMeasure[i + 1].x : mat.cols };
+      return { i, x1: m.x, x2: i < filteredMeasure.length - 1 ? filteredMeasure[i + 1].x : mat.cols, staffs: [[], []] };
     });
+
+  notes.forEach((n) => {
+    const measure = findMeasure(n.x * 2, measures);
+    measures[measure].staffs[0].push(n.notesR);
+    measures[measure].staffs[1].push(n.notesL);
+  });
 
   return measures;
 }
@@ -192,27 +207,15 @@ async function parseSong(song) {
 
   await downloadImages(song);
 
-  const mat = cv.imread('./tmp/output.png');
+  const mat = loadImage('../tmp/output.png');
 
-  const measures = getMeasures(mat.copy());
+  const measures = getMeasures(mat.copy(), notes);
+  // console.log(measures);
 
   const timeSig = getMatchedTemplates(mat, timeSignatures);
-  console.log(timeSig);
+  // console.log(getTimeSignature(timeSig));
 
-  // console.log(measures);
-  // measures.forEach((m) => {
-  // const measure = mat.getRegion(new cv.Rect(m.x1, 0, m.x2 - m.x1, mat.rows));
-  // if (DEBUG) cv.imshow('window', measure);
-  // if (DEBUG) cv.waitKey();
-  // });
-
-  // if (DEBUG) cv.imshow('window', leftCrop);
-  // if (DEBUG) cv.waitKey();
-
-  return;
-
-  const numNotes = 4;
-  for (let i = 0; i < numNotes; i++) {
+  for (let i = 0; i < notes.length; i++) {
     const cropped = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 65, mat.rows)).copy();
 
     const halfHeight = cropped.rows / 2;
@@ -223,24 +226,29 @@ async function parseSong(song) {
       const matches = getMatchedTemplates(bar, templates);
 
       let extraDuration;
-      if (matches.tieLeft) {
-        let newCrop;
-        if (j === 0) newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, halfHeight, 250, halfHeight));
-        else newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 250, halfHeight));
-        extraDuration = handleTies(newCrop);
-      }
+      // if (matches.tieLeft) {
+      //   let newCrop;
+      //   if (j === 0) newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, halfHeight, 250, halfHeight));
+      //   else newCrop = mat.getRegion(new cv.Rect(notes[i].x * 2 - 15, 0, 250, halfHeight));
+      //   extraDuration = handleTies(newCrop);
+      // }
 
       // console.log(matches);
       const duration = getDuration(matches, extraDuration);
       // console.log(duration);
 
-      if (j === 0) notes[i].ld = duration;
-      if (j === 1) notes[i].rd = duration;
+      if (j === 0) notes[i].notesL.forEach((n) => (n.duration = duration));
+      if (j === 1) notes[i].notesR.forEach((n) => (n.duration = duration));
 
       // if (DEBUG) cv.imshow('window', bar);
       // if (DEBUG) cv.waitKey();
     });
   }
+
+  const xml = toXml(measures, { title: song.title, timeSig: getTimeSignature(timeSig) });
+  fs.writeFileSync(path.join(__dirname, '../test.xml'), xml);
+
+  return;
 
   let min = 9999999999;
 
@@ -255,30 +263,37 @@ async function parseSong(song) {
     min = Math.min(min, n.t);
   });
 
-  const rightTrack = new MidiWriter.Track();
-  const leftTrack = new MidiWriter.Track();
+  // const rightTrack = new MidiWriter.Track();
+  // const leftTrack = new MidiWriter.Track();
 
-  rightTrack.setTimeSignature(6, 8);
-  leftTrack.setTimeSignature(6, 8);
+  // rightTrack.setTimeSignature(6, 8);
+  // leftTrack.setTimeSignature(6, 8);
 
-  rightNotes.forEach((n, i) => {
-    // const notes = n.notesR.map((r) => `${r.name.replace('♭', 'b').replace('♯', '#')}${r.octave}`);
-    const notes = n.notesR.map((r) => r.key);
-    if (Array.isArray(n.d)) n.d.forEach((d) => rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
-    else rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
-  });
+  // rightNotes.forEach((n, i) => {
+  //   // const notes = n.notesR.map((r) => `${r.name.replace('♭', 'b').replace('♯', '#')}${r.octave}`);
+  //   const notes = n.notesR.map((r) => r.key);
+  //   if (Array.isArray(n.d)) n.d.forEach((d) => rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
+  //   else rightTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
+  // });
 
-  leftNotes.forEach((n, i) => {
-    // const notes = n.notesL.map((l) => `${l.name.replace('♭', 'b').replace('♯', '#')}${l.octave}`);
-    const notes = n.notesL.map((l) => l.key);
-    if (Array.isArray(n.d)) n.d.forEach((d) => leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
-    else leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
-  });
+  // leftNotes.forEach((n, i) => {
+  //   // const notes = n.notesL.map((l) => `${l.name.replace('♭', 'b').replace('♯', '#')}${l.octave}`);
+  //   const notes = n.notesL.map((l) => l.key);
+  //   if (Array.isArray(n.d)) n.d.forEach((d) => leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: d })));
+  //   else leftTrack.addEvent(new MidiWriter.NoteEvent({ pitch: notes, duration: n.d }));
+  // });
 
-  const write = new MidiWriter.Writer([rightTrack, leftTrack]);
-  write.saveMIDI(song.title);
+  // const write = new MidiWriter.Writer([rightTrack, leftTrack]);
+  // write.saveMIDI(song.title);
 }
 
 // Init and parse song
-const song = JSON.parse(fs.readFileSync('test.json').toString()).data.song;
+if (process.argv.length !== 3) {
+  console.error('usage: node index.js [json file]');
+  return;
+}
+
+console.log('Processing', process.argv[2]);
+
+const song = JSON.parse(fs.readFileSync(process.argv[2]).toString()).data.song;
 parseSong(song);
