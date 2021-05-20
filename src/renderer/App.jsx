d661868cb0e7cb390ead5ae66dee54935ab4ecc0
Andrew McOlash <amcolash@gmail.com>
2021-05-20 15:52:58 -0700

    refactor out sample data

--- src/renderer/App.jsx
+++ src/renderer/App.jsx
@@ -1,33 +1,14 @@
 import React, { useState } from 'react';
 
 import { isDevelopment } from '../common/constants';
+import { sampleData } from '../common/sampleData';
 import { Status } from './Status';
 import { Webview } from './Webview';
-
-const sampleData = {
-  images: [
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/0.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/1.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/2.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/3.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/4.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/5.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/6.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/7.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/8.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/9.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/10.png',
-    'https://flowkeycdn.com/sheets/GzcBWKZdq64xwWPhq/300/11.png',
-  ],
-  id: 'WWSpLW8Xkete2z8mc',
-  title: 'The Sound of Silence',
-  artist: 'Simon & Garfunkel',
-};
 
 const useSampleData = true;
 
 export const App = () => {
-  const [data, setData] = useState(isDevelopment && useSampleData ? sampleData : undefined);
+  const [data, setData] = useState(isDevelopment && useSampleData ? sampleData.SoundOfSilence : undefined);
 
   return (
     <div style={{ width: '100vw', height: '100vh', overflow: 'hidden' }}>
